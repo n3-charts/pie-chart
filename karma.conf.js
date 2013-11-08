d05a8ade64ce5f98c465f5ebd65251038776f03e
Lorem Ipsum <vaporzub@gmail.com>
2013-11-08 20:23:48 +0100

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,36 +1,69 @@
-module.exports = function(config) {
-  config.set({
-    basePath: '',
+// Karma configuration
+// Generated on Mon May 06 2013 08:16:19 GMT+0200 (CEST)
 
-    files: [
-      JASMINE,
-      JASMINE_ADAPTER,
-      'bower_components/angular/angular.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      'bower_components/d3/d3.js',
-      'dist/pie-chart.min.js',
-      'dist/pie-chart.spec.js'
-    ],
 
-    preprocessors: {
-      'dist/pie-chart.min.js': 'coverage'
-    },
+// base path, that will be used to resolve files and exclude
+basePath = '';
 
-    exclude: [],
-    
-    coverageReporter: {
-      type : 'text-summary',
-      dir : 'coverage/'
-    },
 
-    reporters: ['dots', 'coverage'],
+// list of files / patterns to load in the browser
+files = [
+  JASMINE,
+  JASMINE_ADAPTER,
+  'bower_components/angular/angular.js',
+  'bower_components/angular-mocks/angular-mocks.js',
+  'bower_components/d3/d3.js',
+  'dist/pie-chart.min.js',
+  'dist/pie-chart.spec.js'
+];
 
-    port: 9876,
-
-    runnerPort: 9100,
-
-    autoWatch: true,
-    
-    browsers: ['PhantomJS']
-  });
+preprocessors = {
+  'dist/pie-chart.min.js': 'coverage'
 };
+
+// list of files to exclude
+exclude = [
+];
+
+
+coverageReporter = {
+  type : 'text-summary',
+  dir : 'coverage/'
+}
+
+// test results reporter to use
+// possible values: 'dots', 'progress', 'junit'
+reporters = ['dots', 'coverage'];
+
+
+// web server port
+port = 9876;
+
+
+// cli runner port
+runnerPort = 9100;
+
+
+// enable / disable colors in the output (reporters and logs)
+colors = true;
+
+
+// level of logging
+// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+logLevel = LOG_INFO;
+
+
+// enable / disable watching file and executing tests whenever any file changes
+autoWatch = true;
+
+
+// Start these browsers, currently available:
+// - Chrome
+// - ChromeCanary
+// - Firefox
+// - Opera
+// - Safari (only Mac)
+// - PhantomJS
+// - IE (only Windows)
+// browsers = ['PhantomJS'];
+browsers = ['PhantomJS']//, 'Firefox']
